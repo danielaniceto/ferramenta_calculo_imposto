24b3435109cab6a4dd06cb50c98a48bf13eabeb3
danielaniceto <danielgustavots@hotmail.com>
2024-03-06 17:56:15 -0300

    Atividades ate as 17:56

--- api.py
+++ api.py
@@ -15,11 +15,14 @@ def recebe_receita_html():
 def is_CalculoImpostoSimplesNacionalAnexo01_menor180k():
     pega_receita_bruta = request.form["renda_bruta"]
 
-    simples_nacional = SimplesNacional.calcular_simples_nacional_menor_180k(
+    SimplesNacional.calcular_simples_nacional_menor_180k(
         receita_bruta = pega_receita_bruta,
         porcentagem_alicota = 0.04,
         faixa_desconto = 0
         )
+
+def is_apresenta_resultado_simples_nacional_menor_180k():
+    simples_nacional = is_CalculoImpostoSimplesNacionalAnexo01_menor180k
     return render_template("resultado_simples_nacional.html", simples_nacional=simples_nacional)
 
 if __name__ == '__main__':
