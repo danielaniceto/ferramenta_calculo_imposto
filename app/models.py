86d6cecab426b78cad49938d047d737ab6184c37
danielaniceto <danielgustavots@hotmail.com>
2024-03-13 09:35:53 -0300

    Atividades ate as 19:52, dia 12/03

--- app/models.py
+++ app/models.py
@@ -7,12 +7,9 @@ class Cliente:
     self.nome_empresa = nome_empresa
     self.documento = cnpj
 
-  def __str__(self):
-    return json.dumps(self, default=lambda o: o.__dict__,
-      sort_keys=True, indent=4)
-
 class SimplesNacional:
     def __init__(self, receita_bruta, porcentagem_alicota, faixa_desconto):
         self.receita_bruta = receita_bruta
         self.porcentagem_alicota = porcentagem_alicota
         self.faixa_desconto = faixa_desconto
+        
