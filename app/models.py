185e3d2bc95cb198b9ca055d5fbbd84c9f6f134c
danielaniceto <danielgustavots@hotmail.com>
2024-03-25 18:14:18 -0300

    Atividades 25/03, 18:15

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 import uuid
+from typing import List, Dict
 
 class Cliente:
   def __init__(self, id, nome_empresa, cnpj):
@@ -7,6 +8,36 @@ class Cliente:
     self.documento = cnpj
 
 class SimplesNacional:
-    def __init__(self, receita_bruta):
-        self.receita_bruta = float(receita_bruta)
-      
+    
+  TRIBUTACOES: List[Dict[str, int]] = [
+    {"minimo": 0, "maximo": 180000, "aliquota": 0.04, "desconto": 0},
+    {"minimo": 180001, "maximo": 360000, "aliquota": 0.073, "desconto": 5940},
+    {"minimo": 360001, "maximo": 720000, "aliquota": 0.095, "desconto": 13860},
+    {"minimo": 720001, "maximo": 1800000, "aliquota": 0.107, "desconto": 22500},
+    {"minimo": 1800001, "maximo": 3600000, "aliquota": 0.143, "desconto": 87300},
+    {"minimo": 3600001, "maximo": 4800000, "aliquota": 0.19, "desconto": 378000}
+  ]
+   
+  def __init__(self, receita_bruta:float, porcentagem_aliquota:float=None, faixa_desconto:float=None):
+    self.receita_bruta = float(receita_bruta)
+    side_tributacao = self.__get_tributacao_side(self.receita_bruta)
+
+    if porcentagem_aliquota is None:
+      self.porcentagem_aliquota = side_tributacao.get("aliquota")
+
+    else:
+      self.porcentagem_aliquota = porcentagem_aliquota
+            
+    if faixa_desconto is None:
+      self.faixa_desconto = side_tributacao.get("faixa_desconto")
+
+    else:
+       self.faixa_desconto = faixa_desconto
+    
+    @staticmethod
+    def __get_tributacao_side(receita_bruta:float)->dict:
+        for tributacao in SimplesNacional.TRIBUTACOES:
+            if receita_bruta > tributacao["minimo"] and receita_bruta <= tributacao["maximo"]:
+                return tributacao
+        return {}
+    
--- calculos/simples_nacional.py
+++ calculos/simples_nacional.py
@@ -1,46 +1,26 @@
 #from app.models import *
 from typing import List, Dict
+from app.models import SimplesNacional
 
-class CalculoSimplesNacional():
-
-    TRIBUTACOES: List[Dict[str, int]] = [
-        {"minimo": 0, "maximo": 180000, "aliquota": 0.04, "desconto": 0},
-        {"minimo": 180001, "maximo": 360000, "aliquota": 0.073, "desconto": 5940},
-        {"minimo": 360001, "maximo": 720000, "aliquota": 0.095, "desconto": 13860},
-        {"minimo": 720001, "maximo": 1800000, "aliquota": 0.107, "desconto": 22500},
-        {"minimo": 1800001, "maximo": 3600000, "aliquota": 0.143, "desconto": 87300},
-        {"minimo": 3600001, "maximo": 4800000, "aliquota": 0.19, "desconto": 378000}
-    ]
-        
+class CalculoSimplesNacional():        
 #Anexo 01 - Comercio - Calculo para receitas abaixo de R$180.000,00
     @classmethod
-    def calcular_simples_nacional_menor_180k(cls, receita_bruta: float) -> float:
+    def calcular_simples_nacional_menor_180k(cls, receita_bruta: float, porcentagem_aliquota:float=None, faixa_desconto:float=None) -> float:
         print(F"EU SOU A RECEITA DENTRO DE SIMPLES NACIONAL {receita_bruta}")
-        porcentagem_aliquota = None
-        faixa_desconto = None
-        
-        for tributacao in cls.TRIBUTACOES:
-            if receita_bruta > tributacao["minimo"] and receita_bruta <= tributacao["maximo"]:
-                    porcentagem_aliquota = tributacao["aliquota"]
-                    faixa_desconto = tributacao["desconto"]
 
-                    print(f"EU SOU A TRIBUTAÇÃO {tributacao}")
-                    print(f"EU SOU A RECEITA BRUTA DENTRO DA FUNÇÃO {receita_bruta}")
-                    print(f"EU SOU A PORCENTAGEM ALIQUOTA{porcentagem_aliquota}")
-                    print(f"EU SOU A FAIXA DE DESCONTO {faixa_desconto}")
+        print(f"EU SOU A RECEITA BRUTA DENTRO DA FUNÇÃO {receita_bruta}")
 
-            elif receita_bruta < 0:
-                raise Exception("Impossível Calcular Simples Nacional com receita negativa")
+        if receita_bruta < 0:
+            raise Exception("Impossível Calcular Simples Nacional com receita negativa")
             
-            elif receita_bruta not in range(tributacao["minimo"] and receita_bruta in tributacao["maximo"]):
-                raise ValueError("O valor da receita fornecida, está fora do valor máximo de calculo segundo o anexo 01")
+        elif receita_bruta < tributacao["minimo"] or receita_bruta > tributacao["maximo"]:
+            raise ValueError("O valor da receita fornecida, está fora do valor máximo de calculo segundo o anexo 01")
 
-            elif receita_bruta > 0 and receita_bruta <= 180000:
-
-                imposto_anexo01_menor_180 = (float(receita_bruta * porcentagem_aliquota)) - faixa_desconto
-                print(f" EU SOU O IMPOSTO ANEXO 01 {imposto_anexo01_menor_180}")
+        else:
+            imposto_anexo01_menor_180 = (float(receita_bruta * porcentagem_aliquota)) - faixa_desconto
+            print(f" EU SOU O IMPOSTO ANEXO 01 {imposto_anexo01_menor_180}")
                 
-                return round(imposto_anexo01_menor_180, 2)
+            return round(imposto_anexo01_menor_180, 2)
         
 #Anexo 01 - Comercio - Calculo para receitas entre R$180.000,00 a R$360.000,00
 
