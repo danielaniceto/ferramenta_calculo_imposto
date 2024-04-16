db40b34b7b3752f8107baebf6181f668023981b8
danielaniceto <danielgustavots@hotmail.com>
2024-04-16 15:04:53 -0300

    Atividades ate as 15:04, 16/04

--- calculos/simples_nacional.py
+++ calculos/simples_nacional.py
@@ -8,11 +8,4 @@ class CalculoSimplesNacional():
         print(f"EU SOU O IMPOSTO ANEXO 01 = {imposto_simples_nacional}")
                      
         return round(imposto_simples_nacional, 2)
-
-#Anexo 02 - Industria
-        
-#Anexo 03 - Prestadores de Serviço (empresas que oferecem serviços de instalação, de reparos e de manutenção. Consideram-se neste anexo ainda, agências de viagens, escritórios de contabilidade, academias, laboratórios, empresas de medicina e odontologia, consideram-se neste anexo ainda agências de viagens, escritórios de contabilidade, academias, laboratórios, empresas de medicina e odontologia.)
-        
-#Anexo 04 - Prestadores de Serviço (Empresas de Limpeza, vigilancia, obras, construção de imóveis, serviços advocatícios)
-        
-#Anexo 05 - Prestadores de Serviço (empresas que fornecem serviço de auditoria, jornalismo, tecnologia, publicidade, engenharia, entre outros)
+    
