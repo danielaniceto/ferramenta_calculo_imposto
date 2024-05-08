79d322de5bcee714bc3a18a9c4327b99f24dc677
danielaniceto <danielgustavots@hotmail.com>
2024-05-08 11:10:04 -0300

    Atividades ate as 11:10

--- api.py
+++ api.py
@@ -13,9 +13,11 @@ def recebe_receita_html():
     return render_template("escolha_imposto.html")
 
 @app.route('/simples_nacional', methods=['POST'])
-def is_CalculoImpostoSimplesNacional():
+def isApresetaSimplesNacional():
+    return render_template("simples_nacional.html")
 
-    render_template("simples_nacional.html")
+@app.route('/resultado_calculo_imposto_simples_nacional', methods=['POST'])
+def is_CalculoImpostoSimplesNacional():
 
     receita_bruta = float(request.form.get("renda_bruta"))
     print(f"EU SOU A RECEITA BRUTA VINDA DO FORMS = {receita_bruta}")
@@ -28,16 +30,27 @@ def is_CalculoImpostoSimplesNacional():
 
     valor_simples_nacional = SimplesNacional.calcula_simples_nacional(receita_bruta, anexos)
         
-    return render_template ("/resultados_calculos_imposto.html", imposto_simples_nacional = valor_simples_nacional)
+    return render_template ("/resultado_simples_nacional.html", imposto_simples_nacional = valor_simples_nacional)
 
 @app.route('/icms', methods=['POST'])
-def is_CalculoICMS():
+def isApresetaICMS():
+    return render_template("icms.html")
 
-    estado = str(request.form.get("Estados"))
+@app.route('/resultado_calculo_imposto_icms', methods=['POST'])
+def is_CalculoImpostoSimplesNacional():
 
-    return render_template("/icms")
+    receita_bruta = float(request.form.get("renda_bruta"))
+    print(f"EU SOU A RECEITA BRUTA VINDA DO FORMS = {receita_bruta}")
 
+    anexos = str(request.form.get("Anexos_Simples_Nacional"))
+    print(f"EU SOU O ANEXO VINDO DO FORMS = {anexos}")
 
+    validacao_simples_nacional = SimplesNacional(receita_bruta, anexos)
+    print(f"EU SOU O RETONO DA VALIDACAO DO VALOR DO IMPOSTO = {validacao_simples_nacional}")
+
+    valor_simples_nacional = SimplesNacional.calcula_simples_nacional(receita_bruta, anexos)
+        
+    return render_template ("/resultado_simples_nacional.html", imposto_simples_nacional = valor_simples_nacional)
 
 if __name__ == '__main__':
     app.run(host="localhost", port=5000, debug=True)
