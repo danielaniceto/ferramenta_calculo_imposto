a3632b2933fa27e06e8ddbffb39c001664190a99
danielaniceto <danielgustavots@hotmail.com>
2024-05-15 16:09:40 -0300

    Atividades ate as 16:09, 15/05

--- calculos/icms.py
+++ calculos/icms.py
@@ -3,9 +3,9 @@ from typing import Dict
 class CalculoIcms():
 
     @classmethod
-    def calcular_icms(cls, side_tributacao_icms: Dict[str, float]) -> float:
-        imposto_icms = (float(side_tributacao_icms["valor_do_produto"] / side_tributacao_icms["aliquota"]) * side_tributacao_icms["aliquota"])
+    def calcular_icms_dentro_do_estado(cls, side_tributacao_icms: Dict[str, float]) -> float:
+        imposto_icms = float(side_tributacao_icms["valor_produto_servico"] * side_tributacao_icms["aliquota"])
         print(f"EU SOU O IMPOSTO ANEXO 01 = {imposto_icms}")
                      
         return round(imposto_icms, 2)
-    
+        
